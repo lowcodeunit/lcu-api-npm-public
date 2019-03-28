a6c8beb1c33328701527aea2442ae14adcb30219
mcgear <michael.gearhardt@fathym.com>
2019-03-27 22:58:13 -0600

    Fixed search, still need a resolution for faster versions call

--- npm-search/index.js
+++ npm-search/index.js
@@ -1,48 +1,51 @@
-const npmExeq = require("../helpers/npm.execute");
+const npmExeq = require('../helpers/npm.execute');
+const axios = require('axios');
 
 module.exports = async function(context, req) {
-    context.log("NPM Search function processed a request.");
+  context.log('NPM Search function processed a request.');
 
-    var search = req.query.search || (req.body && req.body.search);
+  var search = req.query.search || (req.body && req.body.search);
 
-    var version = req.query.version || (req.body && !!req.body.version) || false;
+  var version = req.query.version || (req.body && !!req.body.version) || false;
 
-    if (search) {
-        try {
-            if (version) {
-                context.log(`Executing NPM Search for ${search} versions...`);
+  if (search) {
+    try {
+      if (version) {
+        context.log(`Executing NPM Search for ${search} versions...`);
 
-                var searchResults = await npmExeq(context, `view ${search} versions --json`);
+        var searchResults = await npmExeq(context, `view ${search} versions --json`);
 
-                console.log(searchResults);
+        console.log(searchResults);
 
-                context.log(`NPM Search complete for ${search} versions:`);
+        context.log(`NPM Search complete for ${search} versions:`);
 
-                context.res = {
-                    body: searchResults
-                };
-            } else {
-                context.log(`Executing NPM Search for ${search}...`);
-
-                var searchResults = await npmExeq(context, `search ${search} --json`);
-
-                context.log(`NPM Search complete for ${search}:`);
-
-                context.res = {
-                    body: searchResults
-                };
-            }
-        } catch (err) {
-            context.log(err);
-            context.res = {
-                status: 400,
-                body: err
-            };
-        }
-    } else {
         context.res = {
-            status: 400,
-            body: "Please pass a search on the query string or in the request body"
+          body: searchResults
         };
+      } else {
+        context.log(`Executing NPM Search for ${search}...`);
+
+        // var searchResults = await npmExeq(context, `search ${search} --json`);
+        
+        const searchResults = await axios.get(`https://api.npms.io/v2/search?q=${search}`);
+
+        context.log(`NPM Search complete for ${search}:`);
+
+        context.res = {
+          body: searchResults
+        };
+      }
+    } catch (err) {
+      context.log(err);
+      context.res = {
+        status: 400,
+        body: err
+      };
     }
+  } else {
+    context.res = {
+      status: 400,
+      body: 'Please pass a search on the query string or in the request body'
+    };
+  }
 };
